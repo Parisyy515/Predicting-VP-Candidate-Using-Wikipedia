8a971201add7d0998595f020f1ec19bfa1c50f2b
Zheng Guo <pariseasterday@gmail.com>
2020-07-19 18:50:17 +0000

    add main function

--- VP_candidate.py
+++ VP_candidate.py
@@ -4,7 +4,7 @@ import operator
 from datetime import *
 
 import bs4
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import requests
 
 
@@ -71,24 +71,32 @@ def each_candidate(url):
 
     occurrences = collections.Counter(R)
 
-    print(name)
-    print(f"The total times is {len(one_week(R))} \n")
-    plt.figure(figsize=(30, 3))
-    plt.title(f'{name} - Editing frequency ')
-    plt.bar(occurrences.keys(), occurrences.values(), align='edge', width=0.3)
+    #print(name)
+    #print(f"The total times is {len(one_week(R))} \n")
+
+    #return_str=name
+    #return_str ="The total times is {len(one_week(R))} \n"
+    #return return_str
+
+    #plt.figure(figsize=(30, 3))
+    #plt.title(f'{name} - Editing frequency ')
+    #plt.bar(occurrences.keys(), occurrences.values(), align='edge', width=0.3)
 
     candidate.append(name)
     cnt.append(len(one_week(R)))
 
 
 def main(url_list, present):
-    print("Please see below the list of democratic VP candidates for the 2020 election, and the total editing times of their Wikipedia page in the most recent one week.\n")
+    return_str = "Please see below the list of democratic VP candidates for the 2020 election, and the total editing times of their Wikipedia page in the most recent one week.\n"
     for url in url_list:
         each_candidate(url)
+        #statement=each_candidate(url)
+        #return_str +=statement
+
 
     allcandidate = dict(zip(candidate, cnt))
     max_key = max(allcandidate, key=allcandidate.get)
-    print(f"\nThe current's time is {present}\n")
-    print(
-        f"Based on the total editing times for each candidate's Wikipedia page in the most recent week, I would like to predict that the next democratic VP is {max_key}\n")
-    print("Below is a chart showing the editing frequency for the most recent 50 edits for each candidate. ")
+    return_str += f"\nThe current's time is {present}\n"
+    return_str += f"Based on the total editing times for each candidate's Wikipedia page in the most recent week, I would like to predict that the next democratic VP is {max_key}\n"
+    #return_str += "Below is a chart showing the editing frequency for the most recent 50 edits for each candidate. "
+    return return_str
--- flask_app.py
+++ flask_app.py
@@ -2,17 +2,17 @@
 # A very simple Flask Hello World app for you to get started with...
 from VP_candidate import main, get_url_list
 from flask import Flask
-from datetime import *
+from datetime import datetime
 
 present = datetime.now()
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    # return 'Hello from Flask!'
+
     url_list = get_url_list()
-    main(url_list, present)
+    statement = main(url_list, present)
+    return statement
 
 if __name__ == "__main__":
     hello_world()
